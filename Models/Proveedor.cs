3aa613156ccde90a16f4704b4d47fba01eb5da75
Diego Higuera <dhiguera09@gmail.com>
2020-03-19 17:24:02 -0500

    Clase proveedor

---
+++ Models/Proveedor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimeraApi.Models
+{
+    class Proveedor{
+        public int id {set;get;}
+        public string Nombre {set;get;}
+        public string Telefono {set;get;}
+    }
+}
